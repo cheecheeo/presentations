f7f456b3cadb2bf0c6bfd4efd9c5eb8a21da17fd
John Chee <cheecheeo@gmail.com>
2015-09-01 18:37:27 -0700

    beamer version of flawless_feature_release_at_enterprise_internet_companies

--- make.sh
+++ make.sh
@@ -3,3 +3,4 @@
 pandoc -t slidy -f markdown+lhs typeclasses.markdown.lhs -o typeclasses.html --self-contained
 pandoc -t slidy -f markdown+lhs just_haskell_or_nothing.markdown.lhs -o just_haskell_or_nothing.html --self-contained
 pandoc -t slidy -f markdown flawless_feature_release_at_enterprise_internet_companies.markdown -o flawless_feature_release_at_enterprise_internet_companies.html --self-contained
+pandoc -t beamer -f markdown flawless_feature_release_at_enterprise_internet_companies.markdown -o flawless_feature_release_at_enterprise_internet_companies.pdf
